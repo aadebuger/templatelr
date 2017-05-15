65d1f73e3aff1a52cddc7593b8ee3f368f73e320
hua zhuang <zh@zqf.com.cn>
2017-05-15 18:21:16 +0800

    more

--- classtemplate.cpp
+++ classtemplate.cpp
@@ -3,6 +3,21 @@ namespace N {
     
 }
 using N::Y;
+template class N::Y<char *>;
+template void N::Y<double>::mf();
+
+template <class T> struct Z
+{
+    void f() {};
+    void g();
+};
+template struct Z<double>;
+Z<int> z1;
+Z<char *> z2;
+
+template <class T> class X;
+// error
+//X<char> Xchar;
 
 int main()
 {
